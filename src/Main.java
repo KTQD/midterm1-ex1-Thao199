08e51917954f6c149eaca46120cae5935febba8d
Thao199 <157661634+Thao199@users.noreply.github.com>
2024-03-14 08:22:34 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
+        MP3 mp3 = new MP3("MP3", "author", "PS50", 61.00f, "4 hours");
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Java", "DLCS", "OOP1", 27.3f, "Pham Phuong Thao", 11122, "reading");
         book.showInfo();
-
     }
 }
